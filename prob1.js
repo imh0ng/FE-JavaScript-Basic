bf3b77ccb70d5eeaef9ffc2a3464240663434945
imhong <ghdgus0417@gmail.com>
2024-04-01 20:36:22 +0900

    answer

--- prob1.js
+++ prob1.js
@@ -1,1 +1,7 @@
 /* 1번 문제 답안을 작성해주세요. */
+
+function solution(num1, num2) {
+    var answer = 0;
+    answer = num1 * num2;
+    return answer;
+}
--- prob2.js
+++ prob2.js
@@ -1,1 +1,24 @@
 /* 2번 문제 답안을 작성해주세요. */
+function factorial(i) {
+    var r = i;
+    for (var a = i; a > 1; a--) {
+        r *= (a-1);
+    }
+    return r;
+}
+
+function solution(n) {
+    var answer = 1;
+    var num = 1;
+    var r = 0;
+    while (1) {
+        r = factorial(answer);
+        if (r == n) break;
+        else if (r > n) {
+            answer -= 1;
+            break;
+        }
+        answer ++;
+    }
+    return answer;
+}
